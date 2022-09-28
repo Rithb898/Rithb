19db72e1fa9802b48e9b6711350aac43284914ea
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2022-09-28 20:04:14 +0530

    Update rithrdp.py

--- rithrdp.py
+++ rithrdp.py
@@ -4,7 +4,7 @@ cmd = 'sudo apt update'
 os.system(cmd)
 cmd = 'sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb'
 os.system(cmd)
-cmd = 'sudo apt install --assume-yes task-kde-desktop desktop-base dbus-x11 xscreensaver'
+cmd = 'sudo apt install --assume-yes  task-gnome-desktop'
 os.system(cmd)
 cmd = 'sudo apt install xfce4-terminal -y'
 os.system(cmd)
