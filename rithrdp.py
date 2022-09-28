020dfd77cd103181954f08330303a92d365cff16
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2022-09-28 13:06:48 +0530

    Create rithrdp.py

---
+++ rithrdp.py
@@ -0,0 +1,21 @@
+import os
+
+cmd = 'sudo apt update'
+os.system(cmd)
+cmd = 'sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb'
+os.system(cmd)
+cmd = 'sudo apt install --assume-yes  task-kde-desktop'
+os.system(cmd)
+cmd = 'sudo apt install xfce4-terminal -y'
+os.system(cmd)
+cmd = 'sudo apt install firefox-esr -y'
+os.system(cmd)
+cmd = 'sudo apt install --assume-yes ./google-chrome-stable_current_amd64.deb'
+os.system(cmd)
+cmd = 'sudo apt-get install geany -y'
+os.system(cmd)
+cmd = 'sudo apt-get install vim-gtk3 -y'
+os.system(cmd)
+cmd = 'sudo apt install iputils-ping -y'
+os.sytem(cmd)
+print("vnc up")
