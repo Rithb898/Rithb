4a7bc13498d4851947ae7d011ce0c887d1bdbc87
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2022-09-30 00:02:46 +0530

    Update rith.py

--- rith.py
+++ rith.py
@@ -10,8 +10,6 @@ cmd = 'wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd
 os.system(cmd)
 cmd = 'sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb'
 os.system(cmd)
-cmd = 'sudo DEBIAN_FRONTEND=noninteractive \'
-os.system(cmd)
 cmd = 'sudo apt install --assume-yes xfce4 desktop-base dbus-x11 xscreensaver'
 os.system(cmd)
 cmd = 'sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'''
@@ -23,7 +21,5 @@ os.system(cmd)
 cmd = 'wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
 os.system(cmd)
 cmd = 'sudo apt install --assume-yes ./google-chrome-stable_current_amd64.deb'
-os.system(cmd)
-cmd = 'sudo apt update'
 os.system(cmd)
 print("vnc up")
