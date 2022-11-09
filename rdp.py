c733a12a44e7de1d4fb1841674a8902fe05aea7e
Rithb898 <105077299+Rithb898@users.noreply.github.com>
2022-11-10 02:38:32 +0530

    Update rdp.py

--- rdp.py
+++ rdp.py
@@ -2,8 +2,6 @@ import os
 
 cmd = 'sudo apt update'
 os.system(cmd)
-cmd = 'sudo apt upgrade -y'
-os.system(cmd)
 cmd = 'sudo apt-get install --assume-yes ./chrome-remote-desktop_current_amd64.deb'
 os.system(cmd)
 cmd = 'sudo apt install --assume-yes  task-kde-desktop'
